ccd06bb59abcc8383f3178039df1e9622663f3f1
DimitriDiel <diel-d@web.de>
2024-06-27 12:34:11 +0200

    Add homework02

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
+
 // Lessons
 import Lesson02 from "./lessons/Lesson02/Lesson02";
 //Homeworks
+import Homework02 from "./homeworks/Homework02/Homework02";
 
 function App() {
   return (
     <div className="App">
       <p>This is first react-app</p>
-
       <Lesson02 />
+      <Homework02 />
     </div>
   );
 }
