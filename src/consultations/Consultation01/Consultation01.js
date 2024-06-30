def9e74fce66fb0d4cd37f2eca34fc321e956303
DimitriDiel <diel-d@web.de>
2024-06-30 19:33:29 +0400

    add Consultation

---
+++ src/consultations/Consultation01/Consultation01.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import bus from "../../assets/bus.jpg";
+
+function Consultation01() {
+  return (
+    <div className="consultation01-components">
+      <img src={bus} alt="bus joke" />
+    </div>
+  );
+}
+
+export default Consultation01;
