19027e0837ee7789b7144a5d2c1668a75bd25f12
DimitriDiel <diel-d@web.de>
2024-07-03 17:53:38 +0400

    add lesson05

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Lesson02 from "./lessons/Lesson02/Lesson02";
 import Lesson03 from "./lessons/Lesson03/Lesson03";
 import Lesson04 from "./lessons/Lesson04/Lesson04";
+import Lesson05 from "./lessons/Lesson05/Lesson05";
 // Homeworks
 import Homework02 from "./homeworks/Homework02/Homework02";
 import Homework03 from "./homeworks/Homework03/Homework03";
@@ -21,7 +22,8 @@ function App() {
       {/* <Lesson03 /> */}
       {/* <Homework03 /> */}
       {/* <Lesson04 /> */}
-      <Homework04 />
+      {/* <Homework04 /> */}
+      <Lesson05 />
     </div>
   );
 }
